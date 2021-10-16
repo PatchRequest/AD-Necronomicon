01157064e9fdb32f383262560a6aa92f08da56b9
PatchRequest <contact@PatchRequest.com>
2021-10-16 17:29:42 +0200

    added licence system
    added licence system

--- main.py
+++ main.py
@@ -6,17 +6,19 @@ from libs.decrypter import RemoteOperations, NTDSHashes
 from libs.utils import parse_target
 
 
-def checkHashPart(username,hashPart,fullHash,target='necronomicon.patchrequest.com',speed="fast"):
-    data = {'username':username,'hash':hashPart,'speed':speed}
+def checkHashPart(username,hashPart,fullHash,target='necronomicon.patchrequest.com',speed="fast",key=""):
+    data = {'username':username,'hash':hashPart,'speed':speed,'key':key}
     answer = requests.post("https://"+target,data=data)
     if answer.text != "null":
-        possibleHashes = json.loads(answer.text)
-        for hash in possibleHashes:
-            if hash['hash'] == fullHash:
-                print("[+] Username: " + username)
-                print("[+] Hash: " + fullHash)
-                print()
-            
+        if answer.status_code == 200:
+            possibleHashes = json.loads(answer.text)
+            for hash in possibleHashes:
+                if hash['hash'] == fullHash:
+                    print("[+] Username: " + username)
+                    print("[+] Hash: " + fullHash)
+                    print()
+        else:
+            print(answer.text)
           
     
   
@@ -37,6 +39,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(add_help = True, description = "Lets take a little peak into the Necronomicon and look if we can find your Active Directory users in it")
     parser.add_argument('target',  help='[[domain/]username[:password]@]<targetName or address>')
+    parser.add_argument('key',  help='The API-Key for the Backend')
     parser.add_argument('--speed',  help='The speed of the queries to the backend [slow,fast]\nslow: Partial hashes are send to the backend -> Slow search in the db \nfast: Full hash is send to backend -> faster search in db', required=False,default="fast")
 
     exec_method = "smbexec"
@@ -79,17 +82,17 @@ if __name__ == "__main__":
 
         print("[-] Processing hashes")
         print("\n")
+        key = args.key.split("=")[1]
         for string in strings:
             parts = string.split(':')
             username = parts[0]
             lmhash = parts[2]
             nthash = parts[3]
-           
             if args.speed == "slow":
                 nthashPart = nthash[0:15]
-                checkHashPart(username,nthashPart,nthash,speed=args.speed)
+                checkHashPart(username,nthashPart,nthash,speed=args.speed,key=key)
             else:
-                checkHashPart(username,nthash,nthash)
+                checkHashPart(username,nthash,nthash,key=key)
 
 
 
